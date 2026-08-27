df09672a723255ddfff988d444bd36c642555f91
17thedevv <traluong196@gmail.com>
2026-08-28 01:54:07 +0700

    style: revamp SupportModal to look premium and modern

--- web_app/src/components/SupportModal.jsx
+++ web_app/src/components/SupportModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { X, MessageCircle, PhoneCall, MessageSquare } from 'lucide-react';
+import { X, MessageCircle, PhoneCall, MessageSquare, ChevronRight } from 'lucide-react';
 import './SupportModal.css';
 
 export default function SupportModal({ isOpen, onClose }) {
@@ -13,31 +13,34 @@ export default function SupportModal({ isOpen, onClose }) {
           <button className="btn-close" onClick={onClose}><X size={20} /></button>
         </div>
         <div className="modal-body">
-          <p className="support-desc">Chọn kênh hỗ trợ phù hợp để được giải đáp nhanh nhất:</p>
+          <p className="support-desc">Bạn cần chúng tôi giúp gì? Đừng ngại chọn một kênh liên hệ nhé!</p>
           
           <div className="support-options">
             <a href="https://zalo.me" target="_blank" rel="noreferrer" className="support-option-card zalo">
-              <div className="support-icon-wrapper"><MessageCircle size={24} /></div>
+              <div className="support-icon-wrapper"><MessageCircle size={28} /></div>
               <div className="support-info">
                 <h4>Hỗ trợ qua Zalo</h4>
                 <p>Phản hồi trong 5 phút</p>
               </div>
+              <ChevronRight className="support-arrow" size={20} />
             </a>
             
             <a href="https://m.me" target="_blank" rel="noreferrer" className="support-option-card messenger">
-              <div className="support-icon-wrapper"><MessageSquare size={24} /></div>
+              <div className="support-icon-wrapper"><MessageSquare size={28} /></div>
               <div className="support-info">
                 <h4>Hỗ trợ qua Messenger</h4>
                 <p>Phản hồi trong 10 phút</p>
               </div>
+              <ChevronRight className="support-arrow" size={20} />
             </a>
             
             <a href="tel:19001560" className="support-option-card hotline">
-              <div className="support-icon-wrapper"><PhoneCall size={24} /></div>
+              <div className="support-icon-wrapper"><PhoneCall size={28} /></div>
               <div className="support-info">
                 <h4>Gọi Hotline (Miễn phí)</h4>
                 <p>1900 1560 (8:00 - 22:00)</p>
               </div>
+              <ChevronRight className="support-arrow" size={20} />
             </a>
           </div>
         </div>
